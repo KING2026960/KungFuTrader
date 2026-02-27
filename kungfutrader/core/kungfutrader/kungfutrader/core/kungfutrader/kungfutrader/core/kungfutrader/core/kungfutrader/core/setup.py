002f1d035096677a54860cdd2d4fa9541292f202
KING2026960 <511713197@QQ.COM>
2026-02-27 08:16:57 +0800

    Add package setup

---
+++ kungfutrader/core/kungfutrader/kungfutrader/core/kungfutrader/kungfutrader/core/kungfutrader/core/kungfutrader/core/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+ with open("README.md", "r", encoding="utf-8") as fh:
+     long_description = fh.read()
+ setup(
+     name="kungfutrader",
+     version="0.1.0",
+     author="KING2026960",
+     author_email="your.email@example.com",
+     description="基于截拳道哲学的轻量级量化交易回测框架",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/KING2026960/KungFuTrader",
+     packages=find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+     python_requires=">=3.8",
+     install_requires=[
+         "pandas>=1.0.0",
+         "numpy>=1.18.0",
+         "yfinance>=0.1.55",
+         "matplotlib>=3.0.0",
+     ],
+ )
